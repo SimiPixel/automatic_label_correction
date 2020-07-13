cea12dd0e986a08806496b2f5e0932d4604b279a
SimiPixel <Simon.Bachhuberr@gmail.com>
2020-07-13 11:23:51 +0200

    First live signs

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+name='ALC',
+version = '0.1.0',
+description = 'A suite of different automated label correction methods',
+url = 'https://github.com/SimiPixel/automatic_label_correction',
+author = 'Simon Bachhuber',
+author_email = 'Simon.Bachhuber@biologie.uni-regensburg.de',
+license = 'MIT',
+packages = setuptools.find_packages(),
+zip_safe = False)
